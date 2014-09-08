756cad022f9ba4af12e7bab6b8112fc4c1d872b0
David VanScott <david.vanscott@gmail.com>
2014-09-08 13:17:58 -0400

    Pull version info from github

--- Anodyne/Web/Controllers/NovaController.php
+++ Anodyne/Web/Controllers/NovaController.php
@@ -1,12 +1,50 @@
 <?php namespace Anodyne\Controllers;
 
-use View;
+use App, View, Github;
 
 class NovaController extends \BaseController {
 
 	public function index()
 	{
-		return View::make('pages.nova.index');
+		// Set an initial version
+		$currentVersion = '2.3.2';
+
+		if (App::environment() == 'production')
+		{
+			// Grab the version
+			$currentVersion = Github::currentRelease('anodyne', 'nova')['name'];
+		}
+
+		$versions = [
+			$currentVersion => $currentVersion,
+			'2.2.3' => '2.2.3',
+			'2.1.3' => '2.1.3',
+			'2.0.3' => '2.0.3',
+			'1.2.6' => '1.2.6',
+		];
+
+		$genres = [
+			''		=> '---',
+			'bl5'	=> "Babylon 5",
+			'bln'	=> "Blank",
+			'bsg'	=> "Battlestar Galactica",
+			'dnd'	=> "Dungeons and Dragons",
+			'dsv'	=> "seaQuest DSV",
+			'sg1'	=> "Stargate SG-1",
+			'sga'	=> "Stargate Atlantis",
+			'baj'	=> "Bajorans (Star Trek)",
+			'crd'	=> "Cardassians (Star Trek)",
+			'ds9'	=> "Deep Space 9 (Star Trek)",
+			'ent'	=> "Enterprise Era (Star Trek)",
+			'kli'	=> "Klingons (Star Trek)",
+			'mov'	=> "Movie Era (Star Trek)",
+			'rom'	=> "Romulans (Star Trek)",
+			'tos'	=> "The Original Series (Star Trek)",
+			'sto'	=> "Star Trek Online",
+		];
+
+		return View::make('pages.nova.index')
+			->with(compact('versions', 'genres'));
 	}
 
 }
