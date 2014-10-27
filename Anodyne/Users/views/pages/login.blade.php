a05ffcd8c105d8ac9e6dfcd282b256926c5456d5
David VanScott <david.vanscott@gmail.com>
2014-10-27 19:47:31 -0400

    Clean up the login page

--- Anodyne/Users/views/pages/login.blade.php
+++ Anodyne/Users/views/pages/login.blade.php
@@ -6,13 +6,16 @@
 
 @section('content')
 	<div class="row">
-		<div class="col-md-8 col-lg-6 col-md-offset-2 col-lg-offset-3">
+		<div class="col-md-8 col-md-offset-2">
 			<h1>Log In</h1>
 
-			<p>Your AnodyneID gives you access to the growing array of Anodyne services and sites, including AnodyneXtras. If you don't have an AnodyneID, you can create one by <a href="{{ route('register') }}">registering</a>.</p>
+			<p>Log in using your AnodyneID below. Once logged in, you'll be logged in across all Anodyne sites and services that use the AnodyneID. If you don't have an AnodyneID, you can get one by {{ link_to_route('register', 'registering') }}.</p>
 
 			<hr class="partial-split">
-
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
 			{{ Form::open(['route' => 'login.do']) }}
 				<div class="form-group{{ ($errors->has('email')) ? ' has-error' : '' }}">
 					<label class="control-label">Email Address</label>
@@ -29,7 +32,7 @@
 				<div class="form-group">
 					{{ Form::button('Log In', ['type' => 'submit', 'class' => 'btn btn-lg btn-block btn-primary']) }}
 					<a href="{{ route('register') }}" class="btn btn-block btn-link">Register Now</a>
-					<a href="{{ route('password.remind') }}" class="btn btn-block btn-link">Forgot Password?</a>
+					<a href="{{ route('password.remind') }}" class="btn btn-block btn-link">Forgot Your Password?</a>
 				</div>
 			{{ Form::close() }}
 		</div>
