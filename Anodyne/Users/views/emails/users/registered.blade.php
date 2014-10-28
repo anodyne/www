ea36b800273977958841165f489f295db9801c9f
David VanScott <david.vanscott@gmail.com>
2014-10-27 20:14:19 -0400

    Clean up the email sent after registering

--- Anodyne/Users/views/emails/users/registered.blade.php
+++ Anodyne/Users/views/emails/users/registered.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	<h1>Welcome to Anodyne Productions!</h1>
 
-	<p>You have successfully registered for your Anodyne Productions account. With your new account, you now have access to AnodyneXtras to search for, download, and submit content. In the future, we'll be launching other services that will use this same account as well.</p>
+	<p>You've successfully registered for your AnodyneID! With your account, you now have access to AnodyneXtras to search for, download, and submit content as well as manage your profile on the Anodyne Productions site. In the future, we'll be launching other services and sites that will use the AnodyneID as well and you'll be able to use this account for those sites as well.</p>
 
 	<p>{{ HTML::link('http://anodyne-productions.com/login', 'Log In Now') }}</p>
 
-	<p>If you believe you've received this message in error, please reply to the email and let us know.</p>
+	<p>If you believe you've received this message in error, please reply and let us know.</p>
 @stop
