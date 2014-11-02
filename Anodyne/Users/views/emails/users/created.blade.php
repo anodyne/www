6d7ece30421cbb95918669b5700bea6aba14701b
David VanScott <david.vanscott@gmail.com>
2014-11-02 11:10:02 -0500

    Update the email views

--- Anodyne/Users/views/emails/users/created.blade.php
+++ Anodyne/Users/views/emails/users/created.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 	<h1>Welcome to Anodyne Productions!</h1>
 
-	<p>An account has been created for you by Anodyne Productions. With your new Anodyne Productions account, you have access to AnodyneXtras to search for, download, and submit content. In the future, we'll be launching other services that will use this same account as well.</p>
+	<p>An AnodyneID has been created for you by Anodyne Productions. With your AnodyneID, you have access to AnodyneXtras to search for, download, and submit content as well as manage your profile on the Anodyne Productions site. In the future, we'll be launching other services and sites that will use the AnodyneID and you'll be able to use this account for those sites as well.</p>
 
-	<p>We have generated a random password for you to start. You should log in and change this password to something easier to remember as soon as possible.</p>
+	<p>We have generated a random password for you to start. You should log in and change this password as soon as possible.</p>
 
 	<p>{{ HTML::link('http://anodyne-productions.com/login', 'Log In Now') }}</p>
 
@@ -14,5 +14,5 @@
 		<li><strong>Password:</strong> {{ $password }}</li>
 	</ul>
 
-	<p>If you believe you've received this message in error, please reply to the email and let us know.</p>
+	<p>If you believe you've received this message in error, please contact Anodyne Productions immediately (contact@anodyne-productions.com).</p>
 @stop
