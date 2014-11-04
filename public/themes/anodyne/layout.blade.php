52c3e021a33b25dd8707f9ad94230744f653dc5e
David VanScott <david.vanscott@gmail.com>
2014-11-04 13:42:13 -0500

    More work on the Wardrobe theme

--- public/themes/anodyne/layout.blade.php
+++ public/themes/anodyne/layout.blade.php
@@ -6,17 +6,21 @@
 		<meta name="description" content="">
 		<meta name="author" content="Anodyne Productions">
 		<meta name="viewport" content="width=device-width">
-		<link rel="icon" type="image/x-icon" href="../favicon.ico?v2">
-		<link rel="apple-touch-icon-precomposed" href="../apple-touch-icon.png">
+		<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico?v1') }}">
+		<link rel="apple-touch-icon-precomposed" href="{{ asset('apple-touch-icon.png') }}">
 
 		<!--[if lt IE 9]>
 		<script type="text/javascript" src="js/html5shiv.js"></script>
 		<![endif]-->
 		
-		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
-		<link href="//fonts.googleapis.com/css?family=Bitter:700" rel="stylesheet">
-		<link href="//fonts.googleapis.com/css?family=Exo+2:500,500italic,600,600italic" rel="stylesheet">
-		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+		@if (App::environment() == 'production')
+			<link href="//fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+			<link href="//fonts.googleapis.com/css?family=Bitter:700" rel="stylesheet">
+			<link href="//fonts.googleapis.com/css?family=Exo+2:500,500italic,600,600italic" rel="stylesheet">
+			<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+		@else
+			<link href="//localhost/global/bootstrap/3.3/css/bootstrap.min.css" rel="stylesheet">
+		@endif
 		{{ HTML::style('css/base.css') }}
 		{{ HTML::style('css/main.css') }}
 		{{ HTML::style('css/responsive.css') }}
@@ -52,12 +56,12 @@
 			<!--[if gte IE 9]><!-->
 				<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 			<!--<![endif]-->
-			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 			<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js"></script>
 			<script src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/additional-methods.min.js"></script>
 		@else
 			<script src="//localhost/global/jquery/jquery-2.1.1.min.js"></script>
-			<script src="//localhost/global/bootstrap/3.2/js/bootstrap.min.js"></script>
+			<script src="//localhost/global/bootstrap/3.3/js/bootstrap.min.js"></script>
 			<script src="//localhost/global/jquery.validate/1.13/jquery.validate.min.js"></script>
 			<script src="//localhost/global/jquery.validate/1.13/additional-methods.min.js"></script>
 		@endif
