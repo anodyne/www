b53941cbc591316e79168a00c7dd55d51a459434
David VanScott <david.vanscott@gmail.com>
2017-03-08 13:29:18 -0500

    2.4.9 release

--- Anodyne/Site/Controllers/NovaController.php
+++ Anodyne/Site/Controllers/NovaController.php
@@ -14,7 +14,7 @@ class NovaController extends \BaseController {
 	public function index()
 	{
 		// Set an initial version
-		$currentVersion = '2.4.8';
+		$currentVersion = '2.4.9';
 
 		if (App::environment() == 'production')
 		{
