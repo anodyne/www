c1752555baf3efad96beee19852d04b22d50aabe
David VanScott <david.vanscott@gmail.com>
2017-07-17 22:43:05 -0400

    Roll back the changes

--- Anodyne/Users/Data/Models/Permission.php
+++ Anodyne/Users/Data/Models/Permission.php
@@ -9,7 +9,7 @@ class Permission extends EntrustPermission {
 	use PresentableTrait;
 	use SoftDeletingTrait;
 
-	protected $table = 'core_permissions';
+	protected $table = 'permissions';
 
 	protected $fillable = ['name', 'display_name'];
 
