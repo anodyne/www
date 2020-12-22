ea741427325ce9547c57d7eecf598d01d7961ba2
David VanScott <david.vanscott@gmail.com>
2020-12-22 15:52:53 -0500

    Updates

--- Anodyne/Site/views/pages/main/service-updates.blade.php
+++ Anodyne/Site/views/pages/main/service-updates.blade.php
@@ -7,21 +7,39 @@
 @section('content')
 	<h1>Upcoming Service Updates</h1>
 
-	<h2>Anodyne website</h2>
+	<p>2021 is going to be an exciting year for Anodyne! We've been making steady progress on Nova 3 and are excited to continue crafting the next generation of RPG management software for the community.</p>
 
-	<p>We have some exciting plans for a total re-imaginging of the Anodyne website. The current branding has been in use for many years now and we're excited to showcase a newer, more modern brand in the coming months. We'll be rolling the new site out in stages over the next few months.</p>
+	<p>Anodyne maintains a whole set of ancillary services that we've maintained for many years now. As time has gone on though, those services have gone into disrepair as we've focused on building Nova 3. In 2021 we want to address as many of those as we possibly can.</p>
 
 	<h2>Forums</h2>
 
-	<p>Effectively immediately, all support will be handled through our <a href="https://discord.gg/7WmKUks" target="_blank">Discord server</a>. The forums have seen less and less traffic over the years and the forum software has fallen significantly behind in updates. Instead of pouring time into the forums, we've made the decision to handle all support through Discord.</p>
+	<p>The first thing we want to discuss is Nova support.</p>
 
-	<p>We have disabled user registrations on the forums, but will leave them there for a few months in case people want to search through the boards for anything. In 2021, we'll be removing the forums altogether.</p>
+	<p>Effectively immediately, all support will be handled through our <a href="https://discord.gg/7WmKUks" target="_blank">Discord server</a>. We've had a Discord server for quite a while now, but haven't actively advertised it until now. Today we pushed updates to all of our sites to provide a link to our Discord from (almost) all pages.</p>
+
+	<p>The truth is that over the years the forums have seen less and less traffic. On top of that, our installation of phpBB is old and would require significant time to get updated to the latest version. Most RPG organization have moved onto Discord anyway, so most of our community is already there, so it made a lot of sense to us to use Discord as the primary means of getting help with Nova.</p>
+
+	<p>We've disabled user registrations on the forums, but will be leaving the actual content active for a few more months in the event people want to search the forums for anything. In the first 90 days of 2021, we'll be removing the site entirely.</p>
 
 	<h2>Help Center</h2>
 
-	<p>The Help Center is going to be folded into a new Anodyne site in the coming months. As such, the site as it stands today will be shut down. The simple fact is that the custom site we built for our own knowledge center needs a lot of updates in order to work as envisioned. Instead of spending time and energy on that, we've decided to go down a different path.</p>
+	<p>Like the forums, the Help Center has been a great source of information for the community, but it's fallen behind with updates and isn't as useful as it could be. The vision for the Help Center was always a place where the community could contribute to documenting how to get the most out of Nova, but bugs have crept into the system and that isn't possible with the way the system is configured.</p>
+
+	<p>Taking a page out of the playbook of popular open source packages, the long-term solution for Nova's documentation will be through the use of a separate GitHub repository. This will provide a straightforward way to contribute back to the documentation through pull requests. (We'll be providing complete documentation on how that process will work from start to finish in the future.)</p>
+
+	<p>The Anodyne staff is currently working on a complete re-write of the Nova 2 documentation to ensure everything is properly documented. We're also heavily focused on writing the documentation in a way that makes it easy for a beginner to get what they need, but also provides advanced topics for people who are themeing and/or modifying Nova. Once that re-write is complete, we'll be folding the documentation directly into the main Anodyne site and shutting down the Help Center in the first half of 2021.</p>
 
 	<h2>AnodyneXtras</h2>
 
-	<p>Like the Help Center, Xtras is going to be folded into a new Anodyne site and re-branded as the Nova Marketplace. This is another application that has fallen into disrepair of late and we want to make it easier for us to do work on this. There will be more information coming in 2021.</p>
+	<p>For years AnodyneXtras has been a one-stop shop for the community to go for finding themes and extensions for Nova. The ease of access for the community is a great way to find tools to get even more out of Nova. For developers, having an avenue of distributing their work is invaluable and eliminates the need to build a site to host their work from. We intend to continue this for the foreseeable future.</p>
+
+	<p>The common theme is that things aren't working the way they're intended to work and that's especially true of AnodyneXtras. This was one of the first services we built and it hasn't been updated since. It's important to us to provide a great experience for the community when it comes to this service, so we think it's time to re-write this and provide the community and developers a better tool. Much like the documentation updates, the new Xtras site will be folded directly into the main Anodyne site to provide a seamless experience for all of our users in 2021.</p>
+
+	<p>We have some exciting future plans for Xtras that we're thrilled to get in your hands, so stay tuned for more information in the coming months.</p>
+
+	<h2>Anodyne website</h2>
+
+	<p>Everything we've talked about here hinges on the main Anodyne site. The current branding has been in use for many years and we're excited to showcase a more modern brand in early 2021. We'll be rolling out the new site in stages over the coming months, starting with the documentation re-write.</p>
+
+	<p>Thank you to everyone for your incredible support over the years. We're excited about what the future holds for Anodyne!</p>
 @stop
