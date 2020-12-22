076d962c78f3b4f84e68e2a3a3cc7e9b006703c4
David VanScott <david.vanscott@gmail.com>
2020-12-22 14:59:02 -0500

    Updates

--- Anodyne/Site/views/pages/main/service-updates.blade.php
+++ Anodyne/Site/views/pages/main/service-updates.blade.php
@@ -7,6 +7,10 @@
 @section('content')
 	<h1>Upcoming Service Updates</h1>
 
+	<h2>Anodyne website</h2>
+
+	<p>We have some exciting plans for a total re-imaginging of the Anodyne website. The current branding has been in use for many years now and we're excited to showcase a newer, more modern brand in the coming months. We'll be rolling the new site out in stages over the next few months.</p>
+
 	<h2>Forums</h2>
 
 	<p>Effectively immediately, all support will be handled through our <a href="https://discord.gg/7WmKUks" target="_blank">Discord server</a>. The forums have seen less and less traffic over the years and the forum software has fallen significantly behind in updates. Instead of pouring time into the forums, we've made the decision to handle all support through Discord.</p>
@@ -15,5 +19,9 @@
 
 	<h2>Help Center</h2>
 
+	<p>The Help Center is going to be folded into a new Anodyne site in the coming months. As such, the site as it stands today will be shut down. The simple fact is that the custom site we built for our own knowledge center needs a lot of updates in order to work as envisioned. Instead of spending time and energy on that, we've decided to go down a different path.</p>
+
 	<h2>AnodyneXtras</h2>
+
+	<p>Like the Help Center, Xtras is going to be folded into a new Anodyne site and re-branded as the Nova Marketplace. This is another application that has fallen into disrepair of late and we want to make it easier for us to do work on this. There will be more information coming in 2021.</p>
 @stop
