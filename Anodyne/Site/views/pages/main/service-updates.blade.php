86301607b3f51382bfb0c0b59e4663f190d910b3
David VanScott <david.vanscott@gmail.com>
2020-12-22 14:53:22 -0500

    Update with forum message

--- Anodyne/Site/views/pages/main/service-updates.blade.php
+++ Anodyne/Site/views/pages/main/service-updates.blade.php
@@ -6,4 +6,14 @@
 
 @section('content')
 	<h1>Upcoming Service Updates</h1>
+
+	<h2>Forums</h2>
+
+	<p>Effectively immediately, all support will be handled through our <a href="https://discord.gg/7WmKUks" target="_blank">Discord server</a>. The forums have seen less and less traffic over the years and the forum software has fallen significantly behind in updates. Instead of pouring time into the forums, we've made the decision to handle all support through Discord.</p>
+
+	<p>We have disabled user registrations on the forums, but will leave them there for a few months in case people want to search through the boards for anything. In 2021, we'll be removing the forums altogether.</p>
+
+	<h2>Help Center</h2>
+
+	<h2>AnodyneXtras</h2>
 @stop
